019c55726c9421d4991eb32b6a3930c1d462c131
Kenzo douchet <kenzodouchet33@gmail.com>
2025-09-29 16:30:20 +0200

    create controller draw + new function

--- src/Controller/DrawController.php
+++ src/Controller/DrawController.php
@@ -8,11 +8,16 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class DrawController extends AbstractController
 {
-    #[Route('/draw', name: 'app_draw')]
+    #[Route('/draw', name: 'draw_all')]
     public function index(): Response
     {
         return $this->render('draw/index.html.twig', [
             'controller_name' => 'DrawController',
         ]);
+    }
+
+    #[Route('/draw/new', name: 'draw_new')]
+    public function new() {
+      dd("test");
     }
 }
