52d91268a5cf351a03dca3c24d4e792e606ec96a
Kenzo douchet <kenzodouchet33@gmail.com>
2025-10-01 15:59:57 +0200

    remove avatar after replace by new

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\ProfileType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $em): Response
     {
+        $user = $this->getUser();
+        $formProfile = $this->createForm(ProfileType::class, $user);
+
+        $formProfile->handleRequest($request);
+
+        if($formProfile->isSubmitted() && $formProfile->isValid()) {
+          $file = $formProfile->get('avatar')->getData();
+          if($file) {
+            $fileSystem = new Filesystem();
+            $oldFile = $this->getParameter('avatar_dir')."/".$user->getAvatar();
+            $fileSystem->remove($oldFile);
+            $newNameFile = uniqid().uniqid().".".$file->guessExtension();
+            $user->setAvatar($newNameFile);
+            $file->move($this->getParameter('avatar_dir'), $newNameFile);
+          }
+          $em->flush();
+          return $this->redirectToRoute("app_profile");
+        }
         return $this->render('profile/index.html.twig', [
-            'controller_name' => 'ProfileController',
+            'formProfile' => $formProfile,
         ]);
     }
 }
