9396813e51fb3162c0d9350cfa1219135a59b9d7
Kenzo douchet <kenzodouchet33@gmail.com>
2025-10-01 12:02:36 +0200

    add avatar in navbar

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -47,5 +47,6 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
+
     }
 }
